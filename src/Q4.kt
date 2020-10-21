84dbe82ac0144aa3290077d6a33753c72c4de201
Syed Waqar Uddin <uddin.8@wright.edu>
2020-10-21 15:54:06 -0400

    Leap year easy way

--- src/Q4.kt
+++ src/Q4.kt
@@ -3,18 +3,24 @@ import java.util.*
 ///// Print the leap year
 fun main() {
 
-    val year = 1900
+    val year = 2004
     var leap:Boolean
 
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
+//    if (year % 4 == 0) {
+//        if (year % 100 == 0) {
+//
+//            leap = year % 400 == 0
+//        } else
+//            leap = true
+//    } else
+//        leap = false
+//
+//    println(if (leap) "$year is a leap year." else "$year is not a leap year.")
 
-            leap = year % 400 == 0
-        } else
-            leap = true
-    } else
-        leap = false
-
-    println(if (leap) "$year is a leap year." else "$year is not a leap year.")
+    if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+        println(" $year is leap year")
+    }else{
+        println(" $year is  not a leap year")
+    }
 }
 
