f991417be7d51964f9fc257b4749c4e1f56d9bec
Syed Waqar Uddin <uddin.8@wright.edu>
2020-10-30 19:02:52 -0400

    New problems added

---
+++ src/Common2Strings.kt
@@ -0,0 +1,33 @@
+
+
+fun main(){
+
+    var s1 = "hello"
+    var s2 = "world"
+
+    twoString(s1,s2)
+
+}
+
+fun twoString( s1:String, s2:String){
+
+//    var duplicate = ""
+//    var notDuplicate = ""
+//
+//    for(i in 0..s1.length-1){
+//        for(j in 0..s2.length-1){
+//            if(s1[i] == s2[j]){
+//               duplicate += "YES"
+//            }else{
+//                notDuplicate += "NO"
+//            }
+//        }
+//    }
+//    if(duplicate.contains("YES")){
+//        println("YES")
+//    }else{
+//        println("NO")
+//    }
+
+
+}
---
+++ src/NewYearChaos.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main(){
+
+    val arr = intArrayOf(5,4,3,2,1)
+    minimumBribes(arr)
+}
+
+
+fun minimumBribes(q:IntArray){
+
+    for(i in 0..q.size){
+
+    }
+}
