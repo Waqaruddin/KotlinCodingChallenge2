d2ab56b8ed1e575defd17ed4d5d4ae2565f6e778
Syed Waqar Uddin <uddin.8@wright.edu>
2020-11-05 15:46:11 -0500

    Singleton in java

---
+++ src/Singleton.java
@@ -0,0 +1,32 @@
+public class Singleton {
+
+    private static Singleton single_instance = null;
+
+    public String s;
+
+    private Singleton(){
+        s = "Hello I am a string of singleton class";
+    }
+
+    public static Singleton getInstance(){
+        if(single_instance == null){
+            single_instance = new Singleton();
+        }
+        return single_instance;
+    }
+}
+
+class Main{
+    public static void main(String args[]){
+        Singleton x = Singleton.getInstance();
+        Singleton y = Singleton.getInstance();
+        Singleton z = Singleton.getInstance();
+
+        x.s = x.s.toUpperCase();
+
+        System.out.println("String from x " + x.s);
+        System.out.println("String from y " + y.s);
+        System.out.println("String from z " + z.s);
+
+    }
+}
